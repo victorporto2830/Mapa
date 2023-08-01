e14575aeb871a9557143503dbf23b381a0265d9c
victorporto2830 <83618124+victorporto2830@users.noreply.github.com>
2023-08-01 23:33:41 +0000

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,39 +15,45 @@ export class AppComponent implements OnInit {
   }
 
   private initMap() {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
+    this.map = L.map('map').setView([0, 0], 2);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
   }
 
   private addSampleMarkers() {
     const sampleMarkers = [
       { 
-      lat: -10.946934,
-      lng: -37.054598,
-      title: 'Parque da Sementeira',
-      description: 'Um belo parque na cidade.'
+        lat: -10.946934,
+        lng: -37.054598,
+        title: 'Parque da Sementeira',
+        description: 'Um belo parque na cidade.'
       },
       {
-      lat: -10.936821,
-      lng: -37.061285,
-      title: 'Museu da Gente Sergipana',
-      description: 'Museu dedicado à cultura sergipana.'
+        lat: -10.936821,
+        lng: -37.061285,
+        title: 'Museu da Gente Sergipana',
+        description: 'Museu dedicado à cultura sergipana.'
       }
     ];
 
-    sampleMarkers.forEach(marker => {
-      const markerIcon = L.icon({
-        iconUrl: 'https://cdn-icons-png.flaticon.com/512/5846/5846044.png',
-        iconSize: [38, 95],
-        iconAnchor: [22, 94],
-        popupAnchor: [-3, -76],
-      });
+    const markerIcon = L.icon({
+      iconUrl: 'https://cdn-icons-png.flaticon.com/512/5846/5846044.png',
+      iconSize: [38, 95],
+      iconAnchor: [22, 94],
+      popupAnchor: [-3, -76],
+    });
 
-      const popupContent = `<b>${marker.title}</b>`;
+    let bounds = L.latLngBounds([]);
+
+    sampleMarkers.forEach(marker => {
+      const popupContent = `<b>${marker.title}</b><br>${marker.description}`;
 
       L.marker([marker.lat, marker.lng], { icon: markerIcon })
         .bindPopup(popupContent)
         .addTo(this.map);
+
+      bounds.extend(L.latLng(marker.lat, marker.lng));
     });
+
+    this.map.fitBounds(bounds);
   }
 }
