aa74d04387a2184f53976391e11870c407097711
victorporto2830 <83618124+victorporto2830@users.noreply.github.com>
2023-08-02 00:02:34 +0000

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
-  selector: 'app-map',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
